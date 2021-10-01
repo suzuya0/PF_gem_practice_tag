ad00668a43a472d15db9f3046a15e3441c0819d2
suzuya0 <sakura240613@gmail.com>
2021-10-01 10:00:14 +0000

    [Add]book投稿機能

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,32 @@
+class BooksController < ApplicationController
+  
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+  
+  def book_params
+    params.require(:book).permit(:title, :content)
+  end
+  
+end
